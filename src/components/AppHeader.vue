00ea290d63169c4869790cddd9f4705d6ce51459
Matteogatto94 <cosenza94@live.it>
2022-11-21 14:44:35 +0100

    add logo and navbar header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,47 @@
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data() {
+        return {
+            menu: [
+                {
+                    text: 'Home',
+                    href: '#',
+                },
+                {
+                    text: 'About',
+                    href: '#'
+                },
+                {
+                    text: 'Services',
+                    href: '#'
+                },
+                {
+                    text: 'Work',
+                    href: '#'
+                },
+                {
+                    text: 'Articles',
+                    href: '#'
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <template>
-    <header>
-        <h1>Head of Page</h1>
+    <header id="site_header" class="d-flex justify-content-between">
+        <div class="logo">
+            <img src="../assets/img/construction_logo_retina.png" alt="">
+        </div>
+
+        <div class="navbar_right">
+            <a :href="item.href" v-for="item in menu" :class="item.text === 'Home' ? 'active' : ''">
+                {{ item.text }}
+            </a>
+            <a class="bg_a" href="#">Get Quote</a>
+        </div>
     </header>
 </template>
 
