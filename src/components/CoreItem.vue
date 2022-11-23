8d48ad881bc4edfef57d858141b1889e57f828de
Matteogatto94 <cosenza94@live.it>
2022-11-23 14:47:17 +0100

    add coreitem components

---
+++ src/components/CoreItem.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+    name: 'CoreItem'
+}
+</script>
+
+<template>
+    <div class="col">
+        <div class="card_core">
+            <span class="icon_core_bg_1">
+                <font-awesome-icon icon="fa-solid fa-house-chimney" class="icon_core bg_1" />
+
+            </span>
+
+            <div class="core_details">
+                <h6 class="py-5 border_core title_yellow_core">Great Services</h6>
+                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                    velit, sed quia</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card_core">
+            <span class="icon_core_bg_2">
+                <font-awesome-icon icon="fa-solid fa-gear" class="icon_core bg_2" />
+            </span>
+
+            <div class="core_details">
+                <h6 class="py-5 border_core title_yellow_core">Highest Standards</h6>
+                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                    velit, sed quia</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card_core">
+            <span class="icon_core_bg_3">
+                <font-awesome-icon icon="fa-solid fa-users" class="icon_core bg_3" />
+            </span>
+
+            <div class="core_details">
+                <h6 class="py-5 border_core title_yellow_core">Professional Team</h6>
+                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                    velit, sed quia</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card_core">
+            <span class="icon_core_bg_4">
+                <font-awesome-icon icon="fa-solid fa-lightbulb" class="icon_core bg_4" />
+            </span>
+
+            <div class="core_details">
+                <h6 class="py-5 border_core title_yellow_core">Professional Team</h6>
+                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                    velit, sed quia</p>
+            </div>
+
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
--- src/components/CoreValues.vue
+++ src/components/CoreValues.vue
@@ -1,6 +1,10 @@
 <script>
+import CoreItem from './CoreItem.vue'
 export default {
-    name: 'CoreValues'
+    name: 'CoreValues',
+    components: {
+        CoreItem
+    }
 }
 </script>
 
@@ -15,61 +19,7 @@ export default {
             </div>
 
             <div class="row">
-                <div class="col">
-                    <div class="card_core">
-                        <span class="icon_core_bg_1">
-                            <font-awesome-icon icon="fa-solid fa-house-chimney" class="icon_core bg_1" />
-
-                        </span>
-
-                        <div class="core_details">
-                            <h6 class="py-5 border_core title_yellow_core">Great Services</h6>
-                            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                                velit, sed quia</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card_core">
-                        <span class="icon_core_bg_2">
-                            <font-awesome-icon icon="fa-solid fa-gear" class="icon_core bg_2" />
-                        </span>
-
-                        <div class="core_details">
-                            <h6 class="py-5 border_core title_yellow_core">Highest Standards</h6>
-                            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                                velit, sed quia</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card_core">
-                        <span class="icon_core_bg_3">
-                            <font-awesome-icon icon="fa-solid fa-users" class="icon_core bg_3" />
-                        </span>
-
-                        <div class="core_details">
-                            <h6 class="py-5 border_core title_yellow_core">Professional Team</h6>
-                            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                                velit, sed quia</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card_core">
-                        <span class="icon_core_bg_4">
-                            <font-awesome-icon icon="fa-solid fa-lightbulb" class="icon_core bg_4" />
-                        </span>
-
-                        <div class="core_details">
-                            <h6 class="py-5 border_core title_yellow_core">Professional Team</h6>
-                            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                                velit, sed quia</p>
-                        </div>
-
-
-                    </div>
-                </div>
+                <CoreItem />
             </div>
         </div>
     </section>
